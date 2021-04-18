330aa991e849e631c38ce70ee488d259f3142ec6
Guruprem Rajpal <57023709+Gurupremrajpal@users.noreply.github.com>
2021-04-18 11:25:53 +0530

    Update MeleeAttack.java

--- src/MeleeAttack.java
+++ src/MeleeAttack.java
@@ -1,19 +1,24 @@
+package Abilities;
 /*
  *author: Guruprem Rajpal
  *date: 22 Mar 2021
  *Description: Strategy
  */
+
+import Monsters.Monster;
+
 public class MeleeAttack implements Attack{
     Monster attacker;
 
     public MeleeAttack(Monster attacker){
         this.attacker = attacker;
+
     }
 
     @Override
     public Integer attack(Monster target) {
         String message = attacker + " uses a melee attack on " + target;
         System.out.println(message);
-        return null;
+        return attacker.getStrength() - target.getDefense();
     }
 }
