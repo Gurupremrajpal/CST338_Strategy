074b619fc0ab14cd290945c11037de2f0b73befd
Guruprem Rajpal <57023709+Gurupremrajpal@users.noreply.github.com>
2021-04-18 11:22:49 +0530

    Update Monster.java

--- src/Monster.java
+++ src/Monster.java
@@ -1,10 +1,14 @@
+package Monsters;
 /*
  *author: Guruprem Rajpal
  *date: 20 Mar 2021
  *Description: Strategy
  */
+import Abilities.Attack;
+
 import java.util.HashMap;
 import java.util.Objects;
+import java.util.Random;
 
 public class Monster {
 
@@ -12,6 +16,70 @@ public class Monster {
     private int xp;
     private int maxHP;
     private HashMap<String, Integer> items;
+    Integer agility=10;
+    Integer defense=10;
+    Integer strength=10;
+    Attack attack;
+
+    public Integer getAgility() {
+        return agility;
+    }
+
+    public Integer getDefense() {
+        return defense;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public Attack getAttack() {
+        return attack;
+    }
+
+    Integer getAttribute(Integer min, Integer max){
+        Random rand = new Random();
+        if(min > max){
+            Integer temp = min;
+            min = max;
+            max = temp;
+        }
+        return rand.nextInt(max-min) + min;
+    }
+
+    Boolean takeDamage(Integer damage){
+        if(damage>=0){
+           hp=hp-damage;
+            System.out.println("The creature was hit for "+damage+" damage");
+        }
+        if(hp<=0){
+            hp=0;
+            System.out.println("Oh no! the creature has perished");
+
+        }
+        System.out.println(this.toString());
+        if(hp>=0) {
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    public Integer attackTarget(Monster target){
+        if (hp<=0){
+            attack.attack(target);
+            target.takeDamage(0);
+            return 0;
+        }
+
+        else {
+            Integer integer = attack.attack(target);
+            target.takeDamage(integer);
+            return integer;
+        }
+    }
 
     public int getHp() {
         return hp;
